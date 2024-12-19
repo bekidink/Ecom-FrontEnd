c87b82dc09ed242dd6e82bd7bc1a96504675ea1a
bekidink <bereketdinku876@gmail.com>
2024-12-19 03:09:35 -0800

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [{ protocol: "https", hostname: "utfs.io" }],
+    remotePatterns: [{ protocol: "https", hostname: "i.pinimg" }],
   },
 };
 
